833e74e8f66fcf9ceff4e3a68e1a44d94caee973
Bakhrullo <kadafiviveo@gmail.com>
2023-11-09 00:57:03 +0500

    mess

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -1,26 +1,17 @@
-from aiogram import types, Dispatcher
-from aiogram.dispatcher import FSMContext
-from aiogram.utils.markdown import hcode
+from aiogram import types, Dispatchercd
+
+from tgbot.misc.i18n import i18ns
+
+_ = i18ns.gettext
+__ = i18ns.lazy_gettext
 
 
 async def bot_echo(message: types.Message):
-    text = [
-        "Эхо без состояния.",
-        "Сообщение:",
-        message.text
-    ]
-
-    await message.answer('\n'.join(text))
+    await message.answer(_("/start tugmasini bosing!"))
 
 
-async def bot_echo_all(message: types.Message, state: FSMContext):
-    state_name = await state.get_state()
-    text = [
-        f'Эхо в состоянии {hcode(state_name)}',
-        'Содержание сообщения:',
-        hcode(message.text)
-    ]
-    await message.answer('\n'.join(text))
+async def bot_echo_all(message: types.Message):
+    await message.answer(_("/start tugmasini bosing!"))
 
 
 def register_echo(dp: Dispatcher):
